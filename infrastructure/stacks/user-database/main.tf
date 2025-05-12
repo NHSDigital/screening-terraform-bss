4e179a994f20bc00ed4c3b30d1de6a72394424d4
Andrew <andrew.cleveland1@nhs.net>
2025-05-12 11:25:58 +0100

    AC - updated terraform database creation

--- infrastructure/stacks/user-database/main.tf
+++ infrastructure/stacks/user-database/main.tf
@@ -1,8 +1,25 @@
 terraform {
+  backend "s3" {
+    bucket       = "screening-bss-terraform-state"
+    key          = "terraform-state/user-database.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
   required_providers {
     postgresql = {
       source  = "cyrilgdn/postgresql"
       version = ">= 1.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Terraform   = "True"
     }
   }
 }
