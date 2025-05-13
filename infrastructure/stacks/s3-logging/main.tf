5a0e6faf1ca5a78baaf8443f39c869dafa7af6c6
Andrew <andrew.cleveland1@nhs.net>
2025-05-13 17:07:44 +0100

    AC - added s3 buckets

---
+++ infrastructure/stacks/s3-logging/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "s3" {
+    bucket       = "screening-bss-terraform-state"
+    key          = "terraform-state/s3.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Terraform   = "True"
+    }
+  }
+}
+
+module "logging_bucket" {
+  source = "./modules/s3"
+  bucket_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "AllowFullAccessToS3Bucket",
+          "Effect" : "Allow",
+          "Principal" : {
+            "AWS" : [
+              "arn:aws:iam::${var.account_id}:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_Admin_443e66bf1656dcb5",
+              "arn:aws:iam::${var.account_id}:role/github-actions-role"
+            ]
+          },
+          "Action" : "s3:*",
+          "Resource" : [
+            "arn:aws:s3:::${var.name_prefix}-${var.bucket_name}",
+            "arn:aws:s3:::${var.name_prefix}-${var.bucket_name}/*"
+          ]
+        }
+      ]
+    }
+  )
+  name_prefix    = var.name_prefix
+  environment    = var.environment
+  name           = var.name
+  bucket_name    = var.bucket_name
+  logging_bucket = null
+}
+
---
+++ infrastructure/stacks/s3-logging/modules/s3/variables.tf
@@ -0,0 +1,31 @@
+variable "environment" {
+  description = "The name of the Environment this is deployed into, for example CICD, NFT, UAT or PROD"
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the resource"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "provides the prefix to keep consistancy"
+  type        = string
+}
+
+variable "logging_bucket" {
+  description = "The bucket where logs are stored for s3 events"
+  type        = string
+  default     = "logging_bucket"
+}
+
+variable "bucket_policy" {
+  description = "The access policy for the bucket"
+  type        = string
+}
+
