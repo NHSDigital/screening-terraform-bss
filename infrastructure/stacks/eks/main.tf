27b137a57d4cd3dea041e05acfe9fb1d59885146
Andrew <andrew.cleveland1@nhs.net>
2025-05-29 13:34:44 +0100

    AC - updated eks subnets

--- infrastructure/stacks/eks/main.tf
+++ infrastructure/stacks/eks/main.tf
@@ -115,7 +115,7 @@ resource "aws_eks_cluster" "cluster" {
 
   vpc_config {
     # subnet_ids              = data.aws_subnets.private_subnets.ids
-    subnet_ids              = [data.aws_subnet.private_subnets.id[0], data.aws_subnet.private_subnets.id[1], data.aws_subnet.public_subnets.id[0], data.aws_subnet.public_subnets.id[1]]
+    subnet_ids              = concat(data.aws_subnets.private_subnets.ids, data.aws_subnets.public_subnets.ids)
     endpoint_private_access = "true"
     endpoint_public_access  = "true"
   }
