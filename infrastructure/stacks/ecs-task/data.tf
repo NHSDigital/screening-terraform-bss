888625e5e03a2c4eb1fac84a90dd6b75e792062f
Jan Hopkins <jan.hopkins1@nhs.net>
2025-05-30 11:35:39 +0100

    added data refs

---
+++ infrastructure/stacks/ecs-task/data.tf
@@ -0,0 +1,26 @@
+data "aws_ecs_cluster" "ecs_cluster" {
+  cluster_name = "${var.name_prefix}${var.cluster_name}"
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.name_prefix}${var.vpc_name}"]
+  }
+}
+
+# Get public subnets
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+  filter {
+    name   = "tag:Environment"
+    values = [var.environment]
+  }
+  filter {
+    name   = "tag:kubernetes.io/role/elb"
+    values = ["1"]
+  }
+}
