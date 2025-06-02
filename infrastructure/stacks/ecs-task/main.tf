6293b16c87cc9bf7ac0bbd0dcbf9b3f5f956999f
stma7 <stephen.macdonald1@nhs.net>
2025-06-02 14:08:44 +0100

    2237 increase cpu memory on task

--- infrastructure/stacks/ecs-task/main.tf
+++ infrastructure/stacks/ecs-task/main.tf
@@ -48,8 +48,8 @@ resource "aws_ecs_task_definition" "task_definition" {
   family                   = "${var.name_prefix}${var.task_name}"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "512"
+  memory                   = "1024"
   execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode(
