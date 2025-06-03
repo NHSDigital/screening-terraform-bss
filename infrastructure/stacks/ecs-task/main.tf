ac6872f0988cd092c05a811536c524d72d300986
Jan Hopkins <jan.hopkins1@nhs.net>
2025-06-03 11:38:42 +0100

    changed module path + cleanup

--- infrastructure/stacks/ecs-task/main.tf
+++ infrastructure/stacks/ecs-task/main.tf
@@ -103,9 +103,9 @@ resource "aws_cloudwatch_log_group" "sample_app_log_group" {
 
 
 module "cw-logs-to-splunk" {
-  source                    = "../stacks/cw-logs-to-splunk"
+  source                    = "../cw-logs-to-splunk"
   cw_log_group_name         = aws_cloudwatch_log_group.sample_app_log_group.name
-  aws_region                = var.aws_region
+  environment               = "cicd"
   # terraform_state_s3_bucket = var.terraform_state_s3_bucket
 }
 
