17fcb7f50ac096b50f2da9b49317b2dfdbd9c8fe
Jan Hopkins <jan.hopkins1@nhs.net>
2025-06-03 11:34:39 +0100

    initial setup cw sub filter stack

--- infrastructure/stacks/cw-logs-to-splunk/data.tf
+++ infrastructure/stacks/cw-logs-to-splunk/data.tf
@@ -7,3 +7,24 @@ data "terraform_remote_state" "cw_firehose_splunk" {
     region = var.aws_region
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket       = "nhse-bss-cicd-state"
+    key          = "terraform-state/firehose-subscription-filter.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Terraform   = "True"
+      Stack       = "SUBSCRIPTION_FILTER"
+    }
+  }
+}
--- infrastructure/stacks/cw-logs-to-splunk/variables.tf
+++ infrastructure/stacks/cw-logs-to-splunk/variables.tf
@@ -7,3 +7,7 @@ variable "filter_pattern" {
   default  = ""
 }
 
+variable "environment" {
+  description = "The name of the Environment this is deployed into, for example CICD, NFT, UAT or PROD"
+  type        = string
+}
