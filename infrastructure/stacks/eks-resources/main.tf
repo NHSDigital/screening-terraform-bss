32df0cd7e67b946069c328a926f1a28dc7952bdb
Andrew <andrew.cleveland1@nhs.net>
2025-06-05 13:45:43 +0100

    AC - updated workflow to include new stack for eks-resources

---
+++ infrastructure/stacks/eks-resources/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  backend "s3" {
+    bucket       = "nhse-bss-cicd-state"
+    key          = "terraform-state/eks-resources.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Terraform   = "True"
+      Stack       = "EKS-RESOURCES"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+locals {
+  cluster_name = "${var.name_prefix}${var.name}"
+}
+
+data "aws_eks_cluster" "eks" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = local.cluster_name
+}
+
+resource "kubernetes_manifest" "alb_ingress_class_params" {
+  manifest = {
+    "apiVersion" = "eks.amazonaws.com/v1"
+    "kind"       = "ingressClassParams"
+    "metadata" = {
+      "name" = "alb"
+    }
+    "spec" = {
+      "scheme" = "internet-facing"
+    }
+  }
+}
+
+resource "kubernetes_ingress_class" "ingress" {
+  metadata {
+    name = "alb"
+    annotations = {
+      "ingressclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  spec {
+    controller = "eks.amazonaws.com/alb"
+    parameters {
+      api_group = "eks.amazonaws.com"
+      kind      = "IngressClassParams"
+      name      = "alb"
+    }
+  }
+}
